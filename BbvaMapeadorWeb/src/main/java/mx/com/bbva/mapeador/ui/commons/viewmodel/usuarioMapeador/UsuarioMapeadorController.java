63c2af9703c33975f3d9724346c4ff7d6fc05a56
jortizsalazar <jortizsalazar@faa56bcd-3277-12e2-d32f-9e1997c5ee66>
2013-05-02 00:23:07 +0000

    Cambio para implementar el patrón invoker

--- BbvaMapeadorWeb/src/main/java/mx/com/bbva/mapeador/ui/commons/viewmodel/usuarioMapeador/UsuarioMapeadorController.java
+++ BbvaMapeadorWeb/src/main/java/mx/com/bbva/mapeador/ui/commons/viewmodel/usuarioMapeador/UsuarioMapeadorController.java
@@ -3,24 +3,29 @@ package mx.com.bbva.mapeador.ui.commons.viewmodel.usuarioMapeador;
 import java.util.List;
 import javax.ejb.EJB;
 
+import mx.com.bbva.bancomer.commons.command.BbvaAbstractCommandInvoker;
+import mx.com.bbva.bancomer.commons.exception.persistence.BbvaBusinessException;
+import mx.com.bbva.bancomer.commons.model.dto.UsuarioDTO;
 import mx.com.bbva.mapeador.ejb.perfil.logicanegocio.PerfilService;
 import mx.com.bbva.mapeador.ejb.perfil.negocio.IPerfil;
 import mx.com.bbva.mapeador.entity.tgm504perfil.Tgm504Perfil;
 import mx.com.bbva.mapeador.ui.commons.viewmodel.usuarioMapeador.UsuarioMapeadorController;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Textbox;
 
-public class UsuarioMapeadorController extends SelectorComposer<Component> {
+public class UsuarioMapeadorController extends SelectorComposer<Component>{
 
 		private static final Logger logger = Logger
 				.getLogger(UsuarioMapeadorController.class);
-		@EJB
-		IPerfil perfilService;
+		@Autowired
+		BbvaAbstractCommandInvoker invoker;
+		
 		@Wire
 		private Textbox identificadorUsuario;
 		
@@ -82,8 +87,31 @@ public class UsuarioMapeadorController extends SelectorComposer<Component> {
 			return cantidadRegistros;
 		}
 		
-		public List<Tgm504Perfil> getPerfiles(){
-			perfilService = new PerfilService();
-			return perfilService.obtenerPerfilesActivo();
+		@SuppressWarnings("unchecked")
+		public List<UsuarioDTO> getPerfiles(){
+			UsuarioDTO usuarioDTO = new UsuarioDTO();
+			List<UsuarioDTO> listaUsuarios = null;
+			try {
+				listaUsuarios = (List<UsuarioDTO>) invoker.invoke(usuarioDTO);
+			} catch (BbvaBusinessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			return listaUsuarios;
 		}
+
+		/**
+		 * @return the invoker
+		 */
+		public BbvaAbstractCommandInvoker getInvoker() {
+			return invoker;
+		}
+
+		/**
+		 * @param invoker the invoker to set
+		 */
+		public void setInvoker(BbvaAbstractCommandInvoker invoker) {
+			this.invoker = invoker;
+		}
+		
 }
