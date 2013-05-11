6f2e0d7bdd99126f2056b23554d661e286e2440b
m1gu3l.angel.martinez.morales <m1gu3l.angel.martinez.morales@gmail.com>
2013-05-11 08:17:30 +0000

    Componente y Pantalla

---
+++ BbvaMapeadorEjb/ejbModule/mappers/componente/MapComponente.java
@@ -0,0 +1,12 @@
+package mappers.componente;
+
+import java.util.List;
+
+import mx.com.bbva.bancomer.bussinnes.model.vo.ComponenteVO;
+
+public abstract interface MapComponente {
+	public abstract List<ComponenteVO> obtenerComponentes(ComponenteVO componenteVO);
+	public abstract void crearComponente(ComponenteVO pantallaVO);
+	public abstract void actualizarComponente(ComponenteVO componenteVO);
+	public abstract void eliminarComponente(ComponenteVO componenteVO);
+}
--- BbvaMapeadorEjb/ejbModule/mappers/pantalla/MapPantalla.java
+++ BbvaMapeadorEjb/ejbModule/mappers/pantalla/MapPantalla.java
@@ -2,9 +2,10 @@ package mappers.pantalla;
 
 import java.util.List;
 
-import mx.com.bbva.bancomer.bussinnes.model.vo.EstatusObjetoVO;
 import mx.com.bbva.bancomer.bussinnes.model.vo.PantallaVO;
 
 public abstract interface MapPantalla {
 	public abstract List<PantallaVO> obtenerPantallas(PantallaVO pantallaVO);
+	public abstract void crearPantalla(PantallaVO pantallaVO);
+	public abstract void actualizarPantalla(PantallaVO pantallaVO);
 }
