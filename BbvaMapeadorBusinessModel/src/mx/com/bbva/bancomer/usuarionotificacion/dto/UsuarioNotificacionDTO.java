8bd980fc2a447663f7d7ba3fca41404d64eea820
m1gu3l.angel.martinez.morales <m1gu3l.angel.martinez.morales@gmail.com>
2013-05-19 05:42:48 +0000

    usuarioNotificacion

---
+++ BbvaMapeadorBusinessModel/src/mx/com/bbva/bancomer/usuarionotificacion/dto/UsuarioNotificacionDTO.java
@@ -0,0 +1,57 @@
+package mx.com.bbva.bancomer.usuarionotificacion.dto;
+
+import java.util.List;
+
+import mx.com.bbva.bancomer.bussinnes.model.vo.EstatusObjetoVO;
+import mx.com.bbva.bancomer.bussinnes.model.vo.UsuarioNotificacionVO;
+import mx.com.bbva.bancomer.commons.model.dto.BbvaAbstractDataTransferObject;
+
+public class UsuarioNotificacionDTO extends BbvaAbstractDataTransferObject   {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4015342575614605865L;
+	private UsuarioNotificacionVO usuarioNotificacionVO;
+	private List<UsuarioNotificacionVO> usuarioNotificacionVOs;
+	private List<EstatusObjetoVO> estatusObjetoVOs;
+	/**
+	 * @return the usuarioNotificacionVO
+	 */
+	public UsuarioNotificacionVO getUsuarioNotificacionVO() {
+		return usuarioNotificacionVO;
+	}
+	/**
+	 * @param usuarioNotificacionVO the usuarioNotificacionVO to set
+	 */
+	public void setUsuarioNotificacionVO(UsuarioNotificacionVO usuarioNotificacionVO) {
+		this.usuarioNotificacionVO = usuarioNotificacionVO;
+	}
+	/**
+	 * @return the usuarioNotificacionVOs
+	 */
+	public List<UsuarioNotificacionVO> getUsuarioNotificacionVOs() {
+		return usuarioNotificacionVOs;
+	}
+	/**
+	 * @param usuarioNotificacionVOs the usuarioNotificacionVOs to set
+	 */
+	public void setUsuarioNotificacionVOs(
+			List<UsuarioNotificacionVO> usuarioNotificacionVOs) {
+		this.usuarioNotificacionVOs = usuarioNotificacionVOs;
+	}
+	/**
+	 * @return the estatusObjetoVOs
+	 */
+	public List<EstatusObjetoVO> getEstatusObjetoVOs() {
+		return estatusObjetoVOs;
+	}
+	/**
+	 * @param estatusObjetoVOs the estatusObjetoVOs to set
+	 */
+	public void setEstatusObjetoVOs(List<EstatusObjetoVO> estatusObjetoVOs) {
+		this.estatusObjetoVOs = estatusObjetoVOs;
+	}
+	
+	
+}
